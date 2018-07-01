515c826e8a470f239675742aff7681f4dd5ee07f
Alex <malachowska.a@gmail.com>
2018-07-01 12:11:16 +0200

    Loop added

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int main()
 {
    cout << "Hello World!"<< endl;
-   cout<< "Hello Coders School"<<endl;
+   for (int i=0; i<10; i++)
+   {
+
+	cout<<i;
+  }
 
    return 0;
 }
